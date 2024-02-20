66266e340971e4474d5981dcb245a88c7ba2ca03
andrey <naumov.a.n@icloud.com>
2024-02-21 00:35:30 +0700

    encoding

--- main.cpp
+++ main.cpp
@@ -12,19 +12,20 @@ void task4();
 
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    setlocale(LC_ALL, "RUS");
+    //SetConsoleCP(1251);
+    //SetConsoleOutputCP(1251);
+    SetConsoleCP(866); SetConsoleOutputCP(866);
+    setlocale(LC_ALL, "Russian");
 
     std::cout << "Homework for the second lecture. Writing to files" << std::endl;
 
     while (true)
     {
-        std::cout << "0: lesson" << std::endl;
-        std::cout << "1: " << std::endl;
-        std::cout << "2: " << std::endl;
-        std::cout << "3: " << std::endl;
-        std::cout << "4: " << std::endl;
+        std::cout << "0: lessonпривет" << std::endl;
+        std::cout << "1: Implement an entry in the accounting statement." << std::endl; //–еализовать запись в ведомость учЄта.
+        std::cout << "2: Implement the drawing of random pictures." << std::endl; //–еализовать рисование случайных картин.
+        std::cout << "3: Implement a simulation of the game \"Fishing\"." << std::endl; //–еализовать симул€цию игры Ђ–ыбалкаї.
+        std::cout << "4: Implement a simulation of ATM operation." << std::endl; //–еализовать симул€цию работы банкомата.
         std::cout << "Enter a number from 0 to 4 or something else to exit: ";
 
         int task;
