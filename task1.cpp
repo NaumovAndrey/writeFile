3f6cd12f7d4fa6554d8a0ae04001fe20ccd6aeec
naumov <106987146+NaumovAndrey@users.noreply.github.com>
2024-02-21 13:29:43 +0700

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,8 +1,31 @@
 #include "iostream"
+#include "fstream"
 
-/**/
+/**/void safeFile(std::string &name,
+                    std::string &surname,
+                    std::string &date,
+                    int &surname){
+    
+}
 
 void task1()
 {
-std::cout << "task1";
+std::string name;
+std::string surname;
+std::string data;
+int salary;
+
+std::cout << "Enter the name";
+std::cin >> name;
+
+std::cout << "Enter the surname";
+std::cin >> surname;
+
+std::cout << "Enter the date";
+std::cin >> date;
+
+std::cout << "Enter the salary";
+srt::cin >> salary;
+
+safeFile(name, surname, date, salary);
 }
