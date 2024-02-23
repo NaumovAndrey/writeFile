979b213d9397acc3e0fe8be58ef5ab6cc42a64b5
andrey <naumov.a.n@icloud.com>
2024-02-23 13:20:51 +0700

    update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,31 +1,39 @@
 #include "iostream"
 #include "fstream"
 
-/**/void safeFile(std::string &name,
-                    std::string &surname,
-                    std::string &date,
-                    int &surname){
-    
+void safeFile(  std::string &name,
+                std::string &surname,
+                std::string &date,
+                int &salary){
+    std::ofstream file;
+    if (!file.good()){
+        file.open("file.txt");
+        file << name << " " << surname << " " << date << " " << salary << std::endl;
+    } else {
+        file.open("file.txt", std::ios::app);
+        file << name << " " << surname << " " << date << " " << salary << std::endl;
+    }
+    std::cout << "update date";
 }
 
 void task1()
 {
 std::string name;
 std::string surname;
-std::string data;
+std::string date;
 int salary;
 
-std::cout << "Enter the name";
+std::cout << "Enter the name: ";
 std::cin >> name;
 
-std::cout << "Enter the surname";
+std::cout << "Enter the surname: ";
 std::cin >> surname;
 
-std::cout << "Enter the date";
+std::cout << "Enter the date: ";
 std::cin >> date;
 
-std::cout << "Enter the salary";
-srt::cin >> salary;
+std::cout << "Enter the salary: ";
+std::cin >> salary;
 
 safeFile(name, surname, date, salary);
 }
